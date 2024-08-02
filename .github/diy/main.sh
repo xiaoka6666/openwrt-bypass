665c55166c3de7773e21a852de0dc60732c53419
xiaoka6666 <115566356+xiaoka6666@users.noreply.github.com>
2024-08-02 14:36:38 +0800

    Update main.sh

--- .github/diy/main.sh
+++ .github/diy/main.sh
@@ -16,7 +16,7 @@ function mvdir() {
 mv -n `find $1/* -maxdepth 0 -type d` ./
 rm -rf $1
 }
-git clone --depth 1 https://github.com/kiddin9/openwrt-packages && mv -n openwrt-packages/luci-app-bypass openwrt-packages/luci-app-fileassistant ./ ; rm -rf openwrt-packages
+git clone --depth 1 https://github.com/kiddin9/openwrt-packages && mv -n openwrt-packages/luci-app-bypass ./ ; rm -rf openwrt-packages
 
 exit 0
 
